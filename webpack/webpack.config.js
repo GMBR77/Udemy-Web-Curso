58d44c036b49ddd9a6308ed0e727f8139f37eb89
GMBR <glaucombribeiro@gmail.com>
2022-05-02 18:36:36 -0300

    aulas 331 - 342 - secao 18  - webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,48 @@
+const modoDev = process.env.NODE_ENV !== 'production'
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+  mode: modoDev ? 'development' : 'production',
+  entry: "./src/principal.js",
+  output: {
+    filename: "principal.js",
+    path: __dirname + "/public",
+  },
+  devServer: {
+    static: {
+        directory: "./public"
+    },
+    compress: true,
+    port: 9000
+},
+  optimization: {
+      minimizer: [
+          new CssMinimizerPlugin({}),
+          new TerserPlugin({
+              parallel: true,
+              terserOptions: {
+                ecma: 6,
+            }
+          })
+      ]
+  },
+  plugins: [
+      new MiniCssExtractPlugin({
+          filename: 'estilo.css'
+      })
+  ],
+  module: {
+    rules: [{
+        test: /\.s?[ac]ss$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            //'style-loader', // Adiciobna CSS a DOM injetando a tag <style>
+            'css-loader', // interpreta @import, url()
+            'sass-loader'
+        ]
+    }],
+  },
+};
